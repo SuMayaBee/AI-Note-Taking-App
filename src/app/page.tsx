5197e535a7311549f3c800fc053018faf53da4b1
Sumaiya Islam <sumaiyaislam-14-2021711385@it.du.ac.bd>
2024-02-17 09:23:05 +0600

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import logo from "@/assets/logo2.png";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+
+export default function Home() {
+  const {userId} = auth()
+
+  if(userId) redirect("/notes")
+
+
+  return(
+    <main className="flex flex-col h-screen items-center justify-center gap-5">
+      <div className="flex items-center gap-4">
+        <Image src={logo} alt="logo" width={100} height={1000} />
+        <span className="font-extrabold tracking-tight text-4xl lg:text-5xl">
+          FlowBrain
+        </span>
+    
+
+      </div>
+      <p className="max-w-prose text-center">
+         
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda iste cupiditate. Iusto neque est veritatis unde ex. Eum voluptas saepe provident facilis dolor ea dolorum quas repellendus optio. Doloremque.
+         
+       </p>
+       <Button size="lg" asChild>
+        <Link href="/notes">
+        Open
+        </Link>
+       </Button>
+
+    </main>
+  )
+}
